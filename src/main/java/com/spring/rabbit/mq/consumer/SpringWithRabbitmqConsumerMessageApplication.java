78d7a617f9d1bec8ed104452281148884a24c0eb
Anderson Lima <anderson.lima@enjoytech.com.br>
2022-02-01 22:41:22 -0300

    primeiro commit

---
+++ src/main/java/com/spring/rabbit/mq/consumer/SpringWithRabbitmqConsumerMessageApplication.java
@@ -0,0 +1,13 @@
+package com.spring.rabbit.mq.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWithRabbitmqConsumerMessageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWithRabbitmqConsumerMessageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/rabbit/mq/consumer/SpringWithRabbitmqConsumerMessageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.rabbit.mq.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWithRabbitmqConsumerMessageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
